af55c341d6bdb3328cd8b00563ae5a5a430dc8c5
nezha <nezha@nezhas-MacBook-Pro.local>
2019-01-04 16:42:24 +0800

    更新较小

--- demo-web/src/main/java/com/nezha/learn/demo/RestApi.java
+++ demo-web/src/main/java/com/nezha/learn/demo/RestApi.java
@@ -1,40 +1,43 @@
 package com.nezha.learn.demo;
 
-import com.nezha.learn.demo.service.LockService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Description: xxxxx <br>
- * @Date: 2018/11/28 9:57 PM <br>
- * @Author: objcat <br>
- * @Version: 1.0 <br>
- */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 
 @RestController
+@RequestMapping(value = "/test")
 public class RestApi {
-    private final static Logger logger = LoggerFactory.getLogger(RestApi.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(RestApi.class);
 
     @Autowired
-    private LockService zkLockService;
-    @RequestMapping(value = "/lock2")
-    public String hello(){
+    private StringRedisTemplate redisTemplate;
+
+    public static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    @RequestMapping(value = "/add")
+    public String addInfo(){
+        atomicInteger.incrementAndGet();
+        logger.info("This bbb's count is:{}",atomicInteger);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd/hh-mm-ss");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String time = formatter.format(localDateTime);
+        String key = "demo:bbb";
         try {
-            zkLockService.acquire();
-            logger.info("2拿到zk锁");
+            redisTemplate.opsForValue().set(key, atomicInteger.toString());
+            redisTemplate.expire(key,10*60,TimeUnit.SECONDS);
         }catch (Exception e){
-            logger.info("2,并发竞争锁失败");
-        }finally {
-            try {
-                zkLockService.release();
-                logger.info("2,释放锁成功了。。。");
-            }catch (Exception e){
-                logger.info("2,释放锁失败");
-            }
         }
-        return "Hello World2";
+        return time;
     }
 }
+
